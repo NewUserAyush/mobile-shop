13b227b4ccfb3186e05a0a1237a145d217494e9c
NewUserAyush <ayushjswl3@gmail.com>
2018-10-18 13:26:22 +0530

    project init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import LandingPage from './components/landingPage/index.js';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <LandingPage/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/landingPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+       <div>Photo slide</div>
+        <div>Category</div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
