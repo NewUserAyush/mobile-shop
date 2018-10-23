47e5a1cf03066175d7895bfcd7a186d65a6ffa08
ayush\Moonraft <ayush@moonraft.com>
2018-10-24 01:08:48 +0530

    change in product file

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -5,7 +5,24 @@ class Product extends React.Component {
   constructor(prop){
     super(prop);
     this.state={
-      arr:[12,2,32,4,1,2,3,4,5,6,7,8,9,10,1,12,12,21,31,2,13,13]
+      arr:[
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+        {"title":"HeadPhone","subtitle":"headphone","img":require('../../asset/headphone2.jpg')},
+      ]
     }
   }
 
@@ -15,11 +32,27 @@ class Product extends React.Component {
         
         <div className="category">
         {this.state.arr.map((d,i) =>
-          <div className="item">{d}</div>
-        )}
+          <ProductItem data = {d}></ProductItem> 
+          )}
         </div>
     );
   }
 }
 
 export default Product;
+
+export const ProductItem = (props) =>
+  {
+            
+                    const styles = {
+                      backgroundImage: `url(${props.data.img})`,
+                      backgroundSize: 'cover',
+                      backgroundRepeat: 'no-repeat'
+                    }
+                return (<div className="item" style={styles}>
+                    <div className="title">{props.data.title}</div>
+                    <div className="subtitle">{props.data.subtitle}</div>
+                </div>);
+                
+                
+}
