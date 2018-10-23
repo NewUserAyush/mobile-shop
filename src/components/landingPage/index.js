c39b8a4f975239029cf614ddf8ed5446d99732a5
Arindam Ganguly <arindam@moonraft.com>
2018-10-24 00:00:28 +0530

    file modified

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Slider from '../slider'
 import './index.css';
 
+let imageList = [1,2,3,4,5,6,7,8,9];
 class LandingPage extends React.Component {
   render() {
     return (
       <div>
-       <div>Photo slide</div>
+       <Slider images = {imageList}></Slider>
         <div>Category</div>
       </div>
     );
